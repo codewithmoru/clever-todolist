1bc868cfeed1ff95bd0f06ace232b92f1f04707b
sevichok <sevichok41294@gmail.com>
2023-03-03 18:00:10 +0300

    pre-commit

--- src/sources/firebase.js
+++ src/sources/firebase.js
@@ -12,6 +12,8 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID
 };
 console.log("sas")
+console.log("sas")
+console.log("sas")
 
 const app = initializeApp(firebaseConfig)
 export const database = getDatabase(app)
